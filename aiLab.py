1f2fadf1304818a0556698697b99831328730893
Hadia Riaz <hadiariaz2006@gmail.com>
2026-02-09 16:37:22 +0500

    added labfile

---
+++ aiLab.py
@@ -0,0 +1,14 @@
+#question no 1
+print("welcome to AI lab")
+#question no 2
+name="hadia"
+semester=2
+print(name)
+print(semester)
+#question no 3
+user_name=input("enter the name")
+print("hello",user_name)
+#question no 4
+num1=int(input("enter the num1"))
+num2=int(input("enter the num2"))
+print("sum:",num1+num2)
